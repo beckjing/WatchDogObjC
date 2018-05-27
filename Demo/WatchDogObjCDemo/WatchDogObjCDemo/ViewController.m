84295d590a77396000768ec5a0d6a4163e060044
beckjing <176547352@qq.com>
2018-05-27 15:31:34 +0800

    first version

---
+++ Demo/WatchDogObjCDemo/WatchDogObjCDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WatchDogObjCDemo
+//
+//  Created by 景悦诚 on 2018/5/27.
+//  Copyright © 2018 NanoSparrow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
